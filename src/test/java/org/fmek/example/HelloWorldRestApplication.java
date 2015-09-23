cc687a0b4805b6c616eab6e54cde1df3a1535258
thma <thma@apache.org>
2015-09-23 13:35:25 +0200

    on the way

--- src/test/java/org/fmek/example/HelloWorldRestApplication.java
+++ src/test/java/org/fmek/example/HelloWorldRestApplication.java
@@ -6,8 +6,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * http://localhost:8080/hello-world/greetings   lists all available Greeting entries
@@ -20,34 +18,14 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 @SpringBootApplication
 public class HelloWorldRestApplication extends ResourceConfig {
 
-  /**
-   * Configure the application. Normally all you would need to do it add sources (e.g.
-   * config classes) because other settings have sensible defaults. You might choose
-   * (for instance) to add default command line arguments, or set an active Spring
-   * profile.
-   *
-   * @param builder a builder for the application context
-   * @return the application builder
-   * @see org.springframework.boot.builder.SpringApplicationBuilder
-   */
-  //@Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    //return super.configure(builder);
-    return builder.sources(HelloWorldRestApplication.class);
-  }
-
     public HelloWorldRestApplication() {
-    register(RequestContextFilter.class);
-    register(HelloWorldResource.class);
-  }
+      register(RequestContextFilter.class);
+      register(HelloWorldResource.class);
+    }
 
   public static void main(String[] args) {
     Object[] contextClasses = {HelloWorldRestApplication.class, EmulateJeeContainerConfiguration.class};
     SpringApplication.run(contextClasses, args);
-
-//    new HelloWorldRestApplication().configure(
-//        new SpringApplicationBuilder(HelloWorldRestApplication.class)).run(args);
-
   }
 
 }
